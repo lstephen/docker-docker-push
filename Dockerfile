6ee3324e49aec768668b8ca5b13d44ca24516892
lestephen <lestephen@groupon.com>
2016-01-08 13:32:43 -0800

    Initial Revision

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM debian:jessie
 MAINTAINER Levi Stephen <levi.stephen@gmail.com>
 
-RUN apt-get update && apt-get -y install git
+RUN apt-get update && apt-get -y install curl git
+
+RUN curl -sSL https://get.docker.com/ | sh
 
 COPY docker-push.sh /usr/local/bin
 RUN chmod +x /usr/local/bin/docker-push.sh
--- docker-push.sh
+++ docker-push.sh
@@ -5,9 +5,50 @@ set -e
 sha=$(git rev-parse --short HEAD)
 name=$DOCKER_PUSH_NAME
 
-function build {
-  docker build -t $name:$sha
+function set_version {
+  version=$(git log --oneline --first-parent master | wc -l | xargs)
+
+  branch=$(git branch | grep '*')
+
+  if [[ ! $branch =~ master$ ]]
+  then
+    branch_count=$[$(git log --oneline --first-parent | wc -l | xargs) - $version]
+    version="$version.dev$branch_count"
+  fi
 }
+
+function setup_ssh {
+  if [[ -d "/ssh" ]]
+  then
+    cp /ssh/* /root/.ssh
+    chmod -R 600 /root/.ssh
+  fi
+}
+
+function build {
+  echo "Building $name:$sha..."
+  docker build -t $name:$sha .
+}
+
+function release {
+  echo "Docker tagging $name:$version..."
+  docker tag -f $name:$sha $name:$version
+  docker tag -f $name:$sha $name:latest
+
+  echo "Pushing $name:$version..."
+  docker login -u "$DOCKER_PUSH_USERNAME" -p "$DOCKER_PUSH_PASSWORD"
+
+  docker push $name:$version
+  docker push $name:latest
+
+  echo "Git tagging $name:$version..."
+  git_tag="v$version"
+  git tag $git_tag
+  git push origin $git_tag
+}
+
+set_version
+setup_ssh
 
 for target in $@
 do
